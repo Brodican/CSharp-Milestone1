093842640466c3fc7104efe5216fe798b034f4f3
Utku Aydin <30289497+Brodican@users.noreply.github.com>
2020-11-20 12:51:01 +0300

    Completed the Guessing Game project.

---
+++ GuessingGame/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace GuessingGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string direction = "";
+            Random rndm = new Random();
+            int guess = 0;
+            int number = 0;
+            number = rndm.Next(1, 20);
+            Console.WriteLine("Guess a number between 1 and 20!");
+
+            while (true)
+            {
+                try
+                {
+                    guess = int.Parse(Console.ReadLine());
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Input a number!");
+                    continue;
+                }
+                
+                if (guess == number)
+                    break;
+
+                if (guess < number)
+                {
+                    direction = "higher";
+                } else
+                {
+                    direction = "lower";
+                }
+
+                Console.WriteLine("Try again! Guess " + direction + " this time.");
+            }
+
+            Console.WriteLine("Well done, you win!");
+        }
+    }
+}
